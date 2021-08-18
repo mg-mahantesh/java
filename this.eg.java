aaee4fa24b2665450e41e43cc3d44ec10681211b
Mahantesh G <65382415+mg-mahantesh@users.noreply.github.com>
2021-08-18 14:56:53 +0530

    Add files via upload
    this keyword in java

---
+++ this.eg.java
@@ -0,0 +1,39 @@
+class sample
+
+{
+    int x;
+    float y;
+    sample(){
+        x=1;
+        y=1.0f;
+    }
+    sample(float a)
+    {
+       this();
+       y=a;
+        
+    }
+    void displayxy()
+    {
+        System.out.println("x = "+x+" y = "+y);
+    }
+}
+class Main{
+    public static void main(String[] args){
+       /* sample s1=new sample();
+        s1.displayxy();*/
+        
+        sample s2=new sample(12.45f);
+        s2.displayxy();
+    }
+}
+
+
+
+
+
+
+
+
+
+
