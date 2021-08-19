1809db962fbc1e742321cc7f4560c9c849696078
Mahantesh G <65382415+mg-mahantesh@users.noreply.github.com>
2021-08-19 12:53:50 +0530

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -28,3 +28,31 @@ class Main
         student s1=new student();
     }
 }
+
+
+
+
+
+/*.....Display the following values for a student using constructor,
+Student name,Marks,address.....*/
+
+
+public class Main {
+    String stname;
+    String staddr;
+    int stmarks;
+    
+    
+  public Main( String name,String addr,int marks) {
+    stname = name;
+    staddr = addr;
+    stmarks=marks;
+    
+  }
+
+  public static void main(String[] args) 
+  {
+    Main obj = new Main("nandakumar", "mysure",85);
+    System.out.println(obj.stname + " " + obj.staddr+" "+obj.stmarks);
+  }
+}
