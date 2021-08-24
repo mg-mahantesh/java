1dcbc2642dc807f19fbfc1df8d538822388a1a4d
Mahantesh G <65382415+mg-mahantesh@users.noreply.github.com>
2021-08-24 10:23:45 +0530

    Create strings.java

---
+++ strings.java
@@ -0,0 +1,25 @@
+/*.....strings in java....*/
+
+import java.io.*;
+import java.lang.*;
+public class Main
+{
+	public static void main(String[] args) {
+	
+	String s1="abc";
+	s1.toUpperCase();
+
+	System.out.println(s1.toLowerCase());
+		
+	System.out.println(s1.charAt(2));
+	    	
+	System.out.println(s1.startsWith("a"));
+	    	
+		System.out.println(s1.endsWith("z"));
+	    
+		System.out.println(s1.replace("a","d"));
+	    	
+	
+	}
+}
+
